3f72bd1d451af88d6d989d52bfbe7364070bc0b6
freshboyswag <vinishko.frede@mail.ru>
2026-09-06 14:07:54 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,20 +1,14 @@
 import discord
 from discord.ext import commands
 import aiohttp
-import importlib
-import sys
-# убедимся что импортируется правильный discord
-import discord
+
 print(f"[Client] discord version: {discord.__version__}", flush=True)
 
 
 class RoleBot(commands.Bot):
     def __init__(self, proxy_url: str, proxy_auth: aiohttp.BasicAuth):
-        intents = discord.Intents.default()
-        intents.members = True
         super().__init__(
             command_prefix="!",
-            intents=intents,
             proxy=proxy_url,
             proxy_auth=proxy_auth,
         )
