ab977883ffe1c9d22ba82b5e05738cfe75ccfde0
freshboyswag <vinishko.frede@mail.ru>
2026-09-06 13:49:28 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,12 +1,18 @@
 import discord
 from discord.ext import commands
+import aiohttp
 
 
 class RoleBot(commands.Bot):
-    def __init__(self):
+    def __init__(self, proxy_url: str, proxy_auth: aiohttp.BasicAuth):
         intents = discord.Intents.default()
         intents.members = True
-        super().__init__(command_prefix="!", intents=intents)
+        super().__init__(
+            command_prefix="!",
+            intents=intents,
+            proxy=proxy_url,
+            proxy_auth=proxy_auth,
+        )
 
     async def setup_hook(self):
         from commands import CheckCog
