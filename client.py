a972127058f0e34cf8956eab7490cd985e075deb
freshboyswag <vinishko.frede@mail.ru>
2026-09-06 04:22:25 +0300

    Update client.py

--- client.py
+++ client.py
@@ -3,17 +3,16 @@ from discord.ext import commands
 
 
 class RoleBot(commands.Bot):
-    def __init__(self, selfbot):
+    def __init__(self):
         intents = discord.Intents.default()
         intents.members = True
         super().__init__(command_prefix="!", intents=intents)
-        self.selfbot = selfbot
 
     async def setup_hook(self):
         from commands import CheckCog
         await self.add_cog(CheckCog(self))
         await self.tree.sync()
-        print("[Bot] Слэш-команды синхронизированы")
+        print("[Bot] Слэш-команды синхронизированы", flush=True)
 
     async def on_ready(self):
-        print(f"[Bot] Залогинен как {self.user}")
+        print(f"[Bot] Залогинен как {self.user}", flush=True)
