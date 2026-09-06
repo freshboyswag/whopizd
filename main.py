a0313040d1cfcd6b4212ba80f42cea0d0647fe78
freshboyswag <vinishko.frede@mail.ru>
2026-09-06 04:10:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import asyncio
 import keepalive
 from config import BOT_TOKEN, USER_TOKEN
-from utils.selfbot import SelfBot
-from bot.client import RoleBot
+from selfbot import SelfBot
+from client import RoleBot
 
 
 async def main():
-    # Запускаем HTTP keepalive сервер для Render + UptimeRobot
     keepalive.start()
-
     selfbot = SelfBot()
     bot = RoleBot(selfbot)
 
-    # Запускаем оба клиента параллельно
     async with asyncio.TaskGroup() as tg:
         tg.create_task(selfbot.start(USER_TOKEN))
         tg.create_task(bot.start(BOT_TOKEN))
